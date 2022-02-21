6d59db0e1f89db00787d784b2cc8f72537c0cce5
swordfatih <accfldekur@gmail.com>
2022-02-21 16:26:05 +0100

    Changes

---
+++ src/main/java/fr/mediaweb/services/Authentification.java
@@ -0,0 +1,52 @@
+package fr.mediaweb.services;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet(name = "Authentification", value = "/")
+public class Authentification extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+
+        // request.setAttribute("nom", request.getParameter("nom"));
+
+        RequestDispatcher view = request.getRequestDispatcher("WEB-INF/templates/authentification.jsp");
+        view.forward(request, response);
+    }
+}
+
+
+/*
+    public static ArrayList<String> connect() throws SQLException, ClassNotFoundException {
+        String url, user, password, req1;
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        url = "jdbc:mysql://tijger.o2switch.net:3306/vmvo1438_mediaweb";
+        user = "vmvo1438_mediaweb";
+        password = "mediaweb4568";
+        Connection conn = DriverManager.getConnection(url, user, password);
+
+        ArrayList<String> utilisateurs = new ArrayList<>();
+        req1 = "SELECT `login` FROM utilisateur";
+        Statement st1 = conn.createStatement();
+        ResultSet RS1 = st1.executeQuery(req1);
+
+        while(RS1.next()) {
+            String login = RS1.getString(1);
+            utilisateurs.add(login);
+        }
+
+        RS1.close();
+        st1.close();
+
+        return utilisateurs;
+    }
+ */
