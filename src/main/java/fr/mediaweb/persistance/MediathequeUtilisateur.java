f7320899faa88cb69d2e1c3a9450c169885b831c
swordfatih <accfldekur@gmail.com>
2022-02-28 16:58:59 +0100

    Fixed cast and syntax error

--- src/main/java/fr/mediaweb/persistance/MediathequeUtilisateur.java
+++ src/main/java/fr/mediaweb/persistance/MediathequeUtilisateur.java
@@ -3,6 +3,8 @@ package fr.mediaweb.persistance;
 import mediatek2022.Mediatheque;
 import mediatek2022.Utilisateur;
 
+import java.util.stream.Collectors;
+
 public class MediathequeUtilisateur implements Utilisateur {
     private final int id;
 	private final String nom;
@@ -28,7 +30,7 @@ public class MediathequeUtilisateur implements Utilisateur {
     public Object[] data() {
         Object[] donnees = new Object[1];
 
-        donnees[0] = Mediatheque.getInstance().tousLesDocumentsDisponibles().stream().map(d -> (MediathequeDocument) d).filter(d -> d.getEmprunt() == id);
+        donnees[0] = Mediatheque.getInstance().tousLesDocumentsDisponibles().stream().map(d -> (MediathequeDocument) d).filter(d -> d.getEmprunt() == id).collect(Collectors.toList());
 
         return donnees;
     }
